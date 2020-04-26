8c6e45d32abef886cbfc73dbb6e41ab5cb2bb528
dcartist <pbannerman13@gmail.com>
2020-04-26 10:05:11 -0400

    refactoring items

--- src/Comp/App/App.js
+++ src/Comp/App/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 import Img from '../Imagelisting/Imagelisting'
 
-function App() {
-  return (
-    <div className="App">
-      Images lising
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {data:[]}
+  }
+  componentWillMount(){
+    axios.get(`https://nameless-wildwood-47841.herokuapp.com/img`)
+    .then(res => {
+    //   const persons = res.data;
+    //   this.setState({ persons });
+        console.log(res.data)
+        this.setState ({data: res.data})
+    })
+}
+
+  render(){
+   return(
+    <div>
       <Img></Img>
     </div>
-  );
+   )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Images lising
+//       <Img></Img>
+//     </div>
+//   );
+// }
 
 export default App;
