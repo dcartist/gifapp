0d2c989ca2244ef03ec4a35498e2069af2fcfce9
dcartist <pbannerman13@gmail.com>
2020-05-27 00:01:34 -0400

    Updating Home

--- src/Comp/Home/Home.js
+++ src/Comp/Home/Home.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-
+import './Home.css'
 class Home extends Component {
     render() {
         return (
-            <div>
-                <p> To help add images to github.</p>
+            <div className="HomeBackground">
+                <div>
+                <h1>Welcome to Giphy insert</h1>
+                <h2>To help add gif comments to github.</h2>
+                <hr></hr>
+                <ul>
+                    <li><b>Images:</b> Shows listing of images added to collection</li>
+                    <li><b>Add Image:</b> Add image (prefferably gif) to the collection</li>
+                    <li><b>Giphy Trends:</b> Shows the trending images on giphy and add them to the collection</li>
+                    <li><b>Giphy Search:</b> Search images on giphy and add them to the collection</li>
+                </ul>
+                <hr></hr>
+                </div>
             </div>
         );
     }
