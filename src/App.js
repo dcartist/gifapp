402ee260a1b335090631164023095ab14c7258b5
Paula Bannerman <Pbannerman13@gmail.com>
2022-08-13 18:43:25 -0400

    uppdating home section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Navigation from './Components/Navigation';
 function App() {
+
+  const [data, setData] = useState({ gifImages: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://nameless-wildwood-47841.herokuapp.com/img/',
+      );
+      console.log(result.data)
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
       <Navigation></Navigation>
+    {/* TESTING
+      {process.env.REACT_APP_API_URL} */}
     </div>
   );
 }
